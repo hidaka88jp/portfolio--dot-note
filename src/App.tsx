1280cba80244b04ad0f3661a2fd653e4e2d40432
hidaka88jp <hidaka88jp@yahoo.co.jp>
2025-04-25 10:28:21 -0700

    feat: add basic Header component structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { Routes, Route } from 'react-router';
 import Home from './pages/Home';
 import Features from './pages/Features';
 import NotFound from './pages/NotFound';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/features' element={<Features />} />
