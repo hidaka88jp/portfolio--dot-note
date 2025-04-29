0f0624231786431ae76a9f202c9ee0481cb7c37e
hidaka88jp <hidaka88jp@yahoo.co.jp>
2025-04-28 17:08:56 -0700

    style: add Inter from Google Fonts

--- src/components/ModeButton/index.tsx
+++ src/components/ModeButton/index.tsx
@@ -3,15 +3,15 @@ import DarkModeIcon from '../../assets/icon_dark_mode.svg';
 export default function ModeButton() {
   return (
     <>
-      <button className='bg-primary flex w-28 max-w-full cursor-pointer items-center justify-start gap-2 rounded-md p-2 text-xs font-bold text-white duration-200 hover:opacity-90 focus:opacity-90 sm:w-36 sm:text-base'>
-        <div className='flex h-4 w-4 items-center justify-center rounded-full bg-white sm:h-6 sm:w-6'>
+      <button className='bg-primary flex w-28 max-w-full cursor-pointer items-center justify-center gap-2 rounded-md p-2 text-xs font-semibold text-white duration-200 hover:opacity-90 focus:opacity-90 sm:w-36 sm:text-base'>
+        <div className='flex h-4 w-4 items-center justify-center rounded-full bg-white sm:h-5 sm:w-5'>
           <img
             src={DarkModeIcon}
             alt='dark mode icon'
-            className='h-2.5 w-2.5 sm:h-4 sm:w-4'
+            className='h-2.5 w-2.5 sm:h-3.5 sm:w-3.5'
           />
         </div>
-        Light Mode
+        Dark Mode
       </button>
     </>
   );
