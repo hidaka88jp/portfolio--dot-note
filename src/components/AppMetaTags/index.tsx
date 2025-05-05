7a42d74f0176d1bac057d5ab26707ab9e597b800
hidaka88jp <hidaka88jp@yahoo.co.jp>
2025-05-05 15:29:36 -0700

    fix: fix theme-color for meta data

--- src/components/AppMetaTags/index.tsx
+++ src/components/AppMetaTags/index.tsx
@@ -8,7 +8,7 @@ export default function AppMetaTags({ isDarkMode }: Props) {
   return (
     <Helmet>
       <title>.note</title>
-      <meta name='theme-color' content={isDarkMode ? '#2d3f2d' : '#f0f2f0'} />
+      <meta name='theme-color' content={isDarkMode ? '#3B523A' : '#F3F4EC'} />
       <meta name='description' content='awesome note app' />
     </Helmet>
   );
