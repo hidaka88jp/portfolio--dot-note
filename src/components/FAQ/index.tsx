43610b20ccea36a4a022c1e67bc1105473f2a6f1
hidaka88jp <hidaka88jp@yahoo.co.jp>
2025-05-08 09:45:38 -0700

    feat: add FAQ component

---
+++ src/components/FAQ/index.tsx
@@ -0,0 +1,11 @@
+import SectionTitle from '../SectionTitle';
+
+export default function FAQ() {
+  return (
+    <>
+      <div className='bg-bg-bottom'>
+        <SectionTitle title='FAQ' />
+      </div>
+    </>
+  );
+}
