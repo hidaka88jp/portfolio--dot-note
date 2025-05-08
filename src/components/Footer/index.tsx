612f0a4be9b86ce79d29a03fa08c9fd416e869cc
hidaka88jp <hidaka88jp@yahoo.co.jp>
2025-05-08 15:57:36 -0700

    feat: add Footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,20 @@
+export default function Footer() {
+  return (
+    <div className='text-primary dark:text-text-light mx-auto flex w-full max-w-96 flex-col items-center justify-center px-5 pt-10 pb-6 text-center text-sm sm:max-w-5xl sm:pt-14'>
+      <p className='mb-5'>© 2025 dot-note inc.</p>
+      <p>
+        Design inspiration: Implemented in React, the UI design inspired by
+        HTML/CSS coding exercises from{' '}
+        <a
+          href='https://moshashugyo.com/lessons/dot-doc'
+          className='underline transition-opacity hover:opacity-80'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Mosha Shugyo
+        </a>
+        .
+      </p>
+    </div>
+  );
+}
