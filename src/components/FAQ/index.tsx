0ef5ce6234e3173ea9ec203a7f725fbadbdbcae3
hidaka88jp <hidaka88jp@yahoo.co.jp>
2025-05-08 11:56:13 -0700

    fix: modify FAQItem component

--- src/components/FAQ/index.tsx
+++ src/components/FAQ/index.tsx
@@ -4,6 +4,23 @@ import BottomWaveSP from '../../assets/faq-wave-top-sp.png';
 import BottomWavePC from '../../assets/faq-wave-top-pc.png';
 
 export default function FAQ() {
+  const faqItems = [
+    {
+      question: 'How can I share my notes?',
+      answer:
+        'Sharing notes is simple. You can set a password and share the link.',
+    },
+    {
+      question: 'Can I back up my notes?',
+      answer: 'Yes, you can back up your notes using the export feature.',
+    },
+    {
+      question: 'Is this service free?',
+      answer:
+        'Yes, the service is completely free during the beta phase.<br /> After the official release, we will offer paid plans as well.',
+    },
+  ];
+
   return (
     <div>
       <img
@@ -19,8 +36,15 @@ export default function FAQ() {
       <div className='bg-bg-bottom'>
         <div className='mx-auto flex w-full max-w-96 flex-col items-center justify-center gap-10 p-5 sm:max-w-5xl sm:gap-20'>
           <SectionTitle title='FAQ' textColor='text-text-light' />
-          <FAQItem />
-          <FAQItem />
+          {faqItems.map((item, index) => (
+            <div className='sm:w-full sm:max-w-xl'>
+              <FAQItem
+                key={index}
+                question={item.question}
+                answer={item.answer}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </div>
--- src/components/FAQItem/index.tsx
+++ src/components/FAQItem/index.tsx
@@ -1,22 +1,26 @@
-export default function FAQItem() {
+type Props = {
+  question: string;
+  answer: string;
+};
+
+export default function FAQItem({ question, answer }: Props) {
   return (
-    <div className='space-y-4'>
-      <div className='flex items-center gap-3.5'>
+    <dl className='space-y-4'>
+      <dt className='flex items-center gap-3.5'>
         <div className='bg-icon-bg-mint flex h-11 w-11 shrink-0 items-center justify-center rounded-full'>
           <span className='text-primary text-xl font-bold'>Q</span>
         </div>
-        <h3 className='text-text-light text-xl font-bold'>
-          How can I share my notes?
-        </h3>
-      </div>
-      <div className='flex items-center gap-3.5'>
+        <span className='text-text-light text-xl font-bold'>{question}</span>
+      </dt>
+      <dd className='flex items-start gap-3.5'>
         <div className='bg-icon-bg-light flex h-11 w-11 shrink-0 items-center justify-center rounded-full'>
           <span className='text-primary text-xl font-bold'>A</span>
         </div>
-        <h3 className='text-text-light'>
-          Sharing notes is simple. You can set a password and share the link.
-        </h3>
-      </div>
-    </div>
+        <span
+          className='text-text-light mt-3'
+          dangerouslySetInnerHTML={{ __html: answer }}
+        />
+      </dd>
+    </dl>
   );
 }
