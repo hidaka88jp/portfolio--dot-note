bcb0ad86a58728143677d206eab2ec13e56c48b5
hidaka88jp <hidaka88jp@yahoo.co.jp>
2025-05-09 11:29:44 -0700

    refactor: replace SVG files with React Icons in CommitmentCard

--- src/components/CommitmentCard/index.tsx
+++ src/components/CommitmentCard/index.tsx
@@ -1,6 +1,6 @@
-import FeaturesIcon from '../../assets/commitment-icon-features.svg';
-import DocumentationIcon from '../../assets/commitment-icon-documentation.svg';
-import EditorIcon from '../../assets/commitment-icon-editor.svg';
+import { MdOutlineSettings } from 'react-icons/md';
+import { CgFileDocument } from 'react-icons/cg';
+import { BiSolidEdit } from 'react-icons/bi';
 
 type Props = {
   icon: 'features' | 'documentation' | 'editor';
@@ -9,16 +9,16 @@ type Props = {
 };
 
 export default function CommitmentCard({ icon, title, description }: Props) {
-  const iconSrc = {
-    features: FeaturesIcon,
-    documentation: DocumentationIcon,
-    editor: EditorIcon,
+  const IconComponent = {
+    features: MdOutlineSettings,
+    documentation: CgFileDocument,
+    editor: BiSolidEdit,
   }[icon];
 
   return (
     <div className='text-primary dark:text-text-light flex w-full max-w-72 flex-col items-center justify-center gap-4'>
       <div className='bg-icon-bg-light flex h-14 w-14 items-center justify-center rounded-full'>
-        <img src={iconSrc} alt='Features' />
+        <IconComponent className='text-secondary h-8 w-8' />
       </div>
       <h3 className='text-lg font-bold'>{title}</h3>
       <p className='text-center text-sm'>{description}</p>
