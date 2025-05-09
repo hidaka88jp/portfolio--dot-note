1577ba15e38aa995545eb238fa2796195c75415b
hidaka88jp <hidaka88jp@yahoo.co.jp>
2025-05-09 10:31:58 -0700

    refactor: Add Link to Header Logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import Logo from '../Logo';
 import ModeButton from '../ModeButton';
+import { Link } from 'react-router';
 
 type Props = {
   isDarkMode: boolean;
@@ -11,7 +12,9 @@ export default function Header({ isDarkMode, toggleDarkMode }: Props) {
     <>
       <div className='bg-bg-top-light dark:bg-bg-top-dark'>
         <div className='mx-auto -mb-[1px] flex w-full max-w-96 items-center justify-between p-5 sm:max-w-5xl'>
-          <Logo />
+          <Link to='/'>
+            <Logo />
+          </Link>
           <ModeButton isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
         </div>
       </div>
