40bc8d3c78db2ce5e98a44655a984e6a47269a3f
hidaka88jp <hidaka88jp@yahoo.co.jp>
2025-05-13 14:24:35 -0700

    fix: update copyright information in footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className='text-primary dark:text-text-light mx-auto flex w-full max-w-96 flex-col items-center justify-center px-5 pt-10 pb-6 text-center text-sm sm:max-w-2xl sm:pt-14 xl:max-w-5xl'>
-      <p className='mb-5'>© 2025 dot-note inc.</p>
+      <p className='mb-5'>© 2025 Takanori Hidaka</p>
       <p>
         Design inspiration: Implemented in React, the UI design inspired by
         HTML/CSS coding exercises from{' '}
